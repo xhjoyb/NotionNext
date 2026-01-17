c1779f1ee4cf7eef9b509bc62741e246708bf617
早清风 <126688600+xhjoyb@users.noreply.github.com>
2026-01-18 01:38:40 +0800

    2

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://www.joyb.cc' },
-    { title: '访问文档中心获取更多帮助', url: 'https://www.joyb.cc/doce' }
+    { title: '访问文档中心获取更多帮助', url: 'https://www.joyb.cc/docs' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -35,7 +35,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://www.loliapi.com/acg/pc/', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -53,7 +53,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   // HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
   // HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://www.joyb.cc',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -117,10 +117,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '交流QQ群',
+  HEO_SOCIAL_CARD_TITLE_2: '加入我们的QQ群讨论分享',
+  HEO_SOCIAL_CARD_TITLE_3: '点击加入Q群',
+  HEO_SOCIAL_CARD_URL: 'https://qm.qq.com/q/CBFtFMeUPm',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
