3d512cd250258bbc529f47b74ae7b09fd411fdbf
早清风 <126688600+xhjoyb@users.noreply.github.com>
2026-01-20 20:28:05 +0800

    Change script inclusion to dynamic JavaScript34553453
    Replaced inline script tag with dynamic script creation.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,7 @@
-<script type="text/javascript" id="myhk" src="https://myhkw.cn/api/player/1690736841113" key="1690736841113" m="1"></script>// 这里编写自定义js脚本；将被静态引入到页面中
+var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/1690736841113");
+script.setAttribute("key","1690736841113");
+script.setAttribute("m","1");
+document.documentElement.appendChild(script);
