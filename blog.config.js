af45bbb7ba91e59676d43e34bda2fea1202dc29a
早清风 <126688600+xhjoyb@users.noreply.github.com>
2026-01-21 12:20:47 +0800

    4324534

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '早清风', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '在世界边缘疯狂的尝试', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.joyb.cc', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.mcpq.cc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '个人博客,日常记录,思维认知,折腾笔记', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/blog-favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '萌ICP备20251117号', // 备案号 闽ICP备XXXXXX
